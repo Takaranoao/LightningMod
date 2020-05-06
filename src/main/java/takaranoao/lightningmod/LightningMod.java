821ed48065a922cab6a79e34dd2435226cbb5d31
Takaranoao <baolan.zifu@gmail.com>
2020-05-07 07:06:14 +0800

    init

---
+++ src/main/java/takaranoao/lightningmod/LightningMod.java
@@ -0,0 +1,10 @@
+package takaranoao.lightningmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LightningMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
